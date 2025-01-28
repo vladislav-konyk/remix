7f211d2ec093eb221b7d6bd5f385585040adabc9
vladislav-konyk <vanarusala@gmail.com>
2025-01-28 22:51:58 +0100

    Initial commit

---
+++ README-to start project.md
@@ -0,0 +1,5 @@
+# Welcome to Remix!
+----- Start to project Remix -----
+1) Win + R - CMD
+2) cd C:\Remix project\remix-app
+3) npm run dev
---
+++ app/root.tsx
@@ -0,0 +1,147 @@
+import { json, redirect } from "@remix-run/node"; 
+import {
+  Form,
+  Links,
+  Meta,
+  NavLink,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "@remix-run/react";
+
+import { useEffect } from "react";
+import { createEmptyContact, getContacts } from "./data";
+import { json, redirect } from "@remix-run/node";
+
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`/contacts/${contact.id}/edit`); 
+};
+
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return json({ contacts, q });
+};
+
+
+import type { LinksFunction,
+  LoaderFunctionArgs,
+ } from "@remix-run/node";
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export default function App() {
+  const { contacts, q } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+  const searching =
+  navigation.location &&
+  new URLSearchParams(navigation.location.search).has(
+    "q"
+  );
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+          <Form
+              id="search-form"
+              onChange={(event) => {
+                const isFirstSearch = q === null;
+                submit(event.currentTarget, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              role="search"
+              >
+                <input
+                  id="q"
+                  aria-label="Search contacts"
+                  className={searching ? "loading" : ""}
+                  defaultValue={q || ""}
+                  placeholder="Search"
+                  type="search"
+                  name="q"
+                />
+                <div
+                aria-hidden
+                hidden={!searching}
+                id="search-spinner"
+              />
+              </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+        <div
+          className={
+            navigation.state === "loading" && !searching
+              ? "loading"
+              : ""
+          }
+          id="detail"
+        >
+          <Outlet />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
